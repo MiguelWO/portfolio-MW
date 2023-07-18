fc844b66a642cfa725529624579b4ae2a87a50db
Miguel Wagner <wmiguel999@gmail.com>
2023-07-18 10:00:11 -0500

    Final Touches

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
         <Tech />
         <Experience />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
