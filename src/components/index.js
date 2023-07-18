0a809e9739bf1b834acd292e0224b9914a775757
Miguel Wagner <wmiguel999@gmail.com>
2023-07-18 07:38:44 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import AboutComponent from './AboutComponent';
+import Tech from './Tech';
+import Experience from './Experience';
+import Works from './Works';
+import Feedbacks from './Feedbacks';
+import Contact from './Contact';
+import ServiceCard from './ServiceCard';
+
+export {
+  Hero,
+  Navbar,
+  AboutComponent,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas, 
+  BallCanvas, 
+  ComputersCanvas, 
+  StarsCanvas,
+  ServiceCard
+}
