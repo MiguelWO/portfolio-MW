15f5c113aa0d2ed9d0f9fc717e0124ad0e3bfa2d
Miguel Wagner <wmiguel999@gmail.com>
2023-07-18 07:59:51 -0500

    Hero Banner Modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,9 +17,8 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Miguel</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I specialize in developing algorithms<br className='sm:block hidden' />
-            to uncover valuable insights from data <br className='sm:block hidden' />
-            insights from data 
+            I specialize in implementing algorithms<br className='sm:block hidden' />
+            to uncover valuable insights from data. 
           </p>
         </div>
       </div>
