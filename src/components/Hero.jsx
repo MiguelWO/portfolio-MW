c38b7228801173eace5f93af12e9e8c747be8925
Miguel Wagner <115993943+MiguelWO@users.noreply.github.com>
2023-07-18 11:33:10 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Miguel</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I specialize in implementing algorithms<br className='sm:block hidden' />
+            I specialize in implementing algorithms <br className='sm:block hidden' />
             to uncover valuable insights from data. 
           </p>
         </div>
